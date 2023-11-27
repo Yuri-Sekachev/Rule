43eee79a277eef4c7515bb6d7f1d15dab6b8d78f
Yuri-Sekachev <yuri.sekachev@rule.se>
2023-11-28 01:19:31 +0200

    Rule project structure

---
+++ config/__init__.py
@@ -0,0 +1,8 @@
+from config.rule_url import Url
+from config.playwright import Playwright
+from config.expectations import Expectations
+
+
+url = Url()
+playwright = Playwright()
+expectations = Expectations()
